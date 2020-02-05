00071cad647aa9c0c8642eb4d477efc3f7ae5a53
meira <56406951+meira@users.noreply.github.com>
2020-02-05 11:33:03 -0300

    Programa para decifrar mensagens criptografadas.

---
+++ programa.py
@@ -0,0 +1,47 @@
+import requests
+import json 
+import hashlib
+
+request = requests.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=12768e626035a6d4296b9e316aa251154397643e')
+
+json_dados = json.loads(request.content)
+
+print(json_dados)
+
+caracteres = 'abcdefghijklmnopqrstuvwxyz'
+chave = json_dados["numero_casas"]
+mensagem_cifrada = json_dados["cifrado"]
+mensagem_decifrada = ''
+
+for caractere in mensagem_cifrada:
+    if caractere in caracteres:
+        numero = caracteres.find(caractere)        
+        numero = numero - chave
+        if numero >= len(caracteres):
+            numero = numero - len(caracteres)
+            mensagem_decifrada = mensagem_decifrada + caracteres[numero]
+        elif numero < 0:
+            numero = numero + len(caracteres)
+            mensagem_decifrada = mensagem_decifrada + caracteres[numero]
+        else:
+            mensagem_decifrada = mensagem_decifrada + caracteres[numero]
+    else:
+         mensagem_decifrada = mensagem_decifrada + ' '
+
+print('O texto decifrado é ', mensagem_decifrada)
+
+
+sha1 = hashlib.sha1(mensagem_decifrada.encode())
+json_dados["resumo_criptografico"] = sha1.hexdigest()
+print(json_dados)
+
+with open("answer.json", "w") as f:
+    json.dump(json_dados, f, ensure_ascii=False, indent=4)
+
+
+answer = {'answer': ('answer.json', open("answer.json", "rb"))}
+
+
+request = requests.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=12768e626035a6d4296b9e316aa251154397643e')
+
+print(request.json())
